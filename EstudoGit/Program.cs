2c337bbc63a108d3ac65d37097b344c40ec0aa60
Renato Zaha <renato.zaha@gmail.com>
2022-02-07 17:00:33 -0300

    adicionado como voce esta

--- EstudoGit/Program.cs
+++ EstudoGit/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("bom dia");
+Console.WriteLine("como voce está");
 Console.ReadLine();
 
